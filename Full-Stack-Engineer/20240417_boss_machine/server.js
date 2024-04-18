a3be00856a950a30a2defe1ee6b27f5ddc16004a
Peter Key <63628694+peterekey@users.noreply.github.com>
2024-04-18 22:03:33 +1000

    Boss machine - make changes to index.html and server.js to make the page load in the browser

--- Full-Stack-Engineer/20240417_boss_machine/server.js
+++ Full-Stack-Engineer/20240417_boss_machine/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-
 module.exports = app;
 
 /* Do not change the following line! It is required for testing and allowing
@@ -9,17 +8,28 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
+const cors = require('cors');
+app.use(cors());
 
+// Add middleware for parsing request bodies here:
+const bodyParser = require('body-parser');
 
-// Add middware for parsing request bodies here:
+// Need to serve index.html directly from the route with a specific route for the root URL:
+const path = require('path');
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+  })
 }
